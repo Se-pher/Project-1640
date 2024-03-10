ffee8e5e2a50327266c75f77528b9edf645c1b30
Tannguyen29 <tannvgdh210106@fpt.edu.vn>
2024-03-10 15:13:27 +0700

    Update App.js

--- project1640/src/App.js
+++ project1640/src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Home from "./Components/LandingPage"
-import Login from "./Components/Login"
-import Register from './Components/Register';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Components/LandingPage";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 
 function App() {
   return (
     <Router>
-    {/* <Header /> */}
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
