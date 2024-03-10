6afb1408eb4592fa75f533da0220ce1e24d52e03
Tannguyen29 <tannvgdh210106@fpt.edu.vn>
2024-03-10 15:11:36 +0700

    Register

--- project1640/src/App.js
+++ project1640/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./Components/LandingPage"
 import Login from "./Components/Login"
+import Register from './Components/Register';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   </Router>
   );
